367f6f2af19dff19fd478031affc6bf54fb313bd
jinhoLee <kswlsgh@naver.com>
2023-02-28 23:07:49 +0900

    v5

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -10,23 +10,22 @@ import java.util.Map;
 
 public class MyView {
     private String viewPath;
-
     public MyView(String viewPath) {
         this.viewPath = viewPath;
     }
-
-    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void render(HttpServletRequest request, HttpServletResponse
+            response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
-        dispatcher.forward(request,response);
+        dispatcher.forward(request, response);
     }
-
-    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void render(Map<String, Object> model, HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
         modelToRequestAttribute(model, request);
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
-        dispatcher.forward(request,response);
+        dispatcher.forward(request, response);
     }
-
-    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
-        model.forEach((key, value)-> request.setAttribute(key,value));
+    private void modelToRequestAttribute(Map<String, Object> model,
+                                         HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
